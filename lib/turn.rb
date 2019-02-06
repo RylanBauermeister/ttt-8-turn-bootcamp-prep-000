6fe22d2a87ed5dd6aeb558e83b093aeb2ee8d7b1
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-06 05:33:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,14 @@
-def turn
-  
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
 
 def valid_move?(board, index)
   index >= 0 && index < 10 && !position_taken?(board, index)
@@ -8,3 +17,10 @@ end
 def position_taken?(board, index)
   board[index].to_s.strip != ""
 end
+
+def move(board, index, type = 'X')
+  if valid_move?(index)
+    board[index] = 'X'
+  end
+end
+    
