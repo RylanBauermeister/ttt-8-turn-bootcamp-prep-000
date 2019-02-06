9ad4dc7ddaaf9ee72e99f3bd9b4e79bfcb68d0e6
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-06 05:30:18 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,1 +1,10 @@
 def turn
+  
+
+def valid_move?(board, index)
+  index >= 0 && index < 10 && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  board[index].to_s.strip != ""
+end
